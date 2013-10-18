a0e9fc3fec08a5c0b9eeb3889e3b58351aac7e3f
Nykolas Lima <nykolas.lima@gmail.com>
2013-10-18 02:02:59 -0300

    adding tests to unsupported methods

--- src/test/java/br/com/caelum/vraptor/i18n/TranslatorTest.java
+++ src/test/java/br/com/caelum/vraptor/i18n/TranslatorTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -17,10 +18,15 @@ public class TranslatorTest {
 	
 	@Mock
 	private Localization localization;
+	private Translator map;
+	
+	@Before
+	public void setUp() {
+		map = new Translator(localization);
+	}
 	
 	@Test
 	public void should_return_span_if_key_does_not_exist() {
-		Translator map = new Translator(localization);
 		when(localization.getMessage("must_be_not_empty")).thenReturn("???must_be_not_empty???");
 		assertFalse(map.containsKey("must_be_not_empty"));
 		assertEquals("<span class='i18n_missing_key'>must_be_not_empty</span>", map.get("must_be_not_empty").toString());
@@ -30,7 +36,6 @@ public class TranslatorTest {
 	public void should_return_key_if_it_exists() {
 		String message = "Must be not empty";
 
-		Translator map = new Translator(localization);
 		when(localization.getMessage("must_be_not_empty")).thenReturn(message);
 		assertTrue(map.containsKey("must_be_not_empty"));
 		assertEquals(message, map.get("must_be_not_empty").toString());
@@ -40,9 +45,53 @@ public class TranslatorTest {
 	public void should_return_interpolated_string() {
 		String message = "Field {0} must be not empty for component {1}";
 
-		Translator map = new Translator(localization);
 		when(localization.getMessage("must_be_not_empty")).thenReturn(message);
 		assertEquals("Field name must be not empty for component User", map.get("must_be_not_empty").args("name", "User").toString());
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void containsValueShouldThrowException() {
+		map.containsValue(null);
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void entrySetShouldThrowException() {
+		map.entrySet();
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void isEmptyShouldThrowException() {
+		map.isEmpty();
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void keySetShouldThrowException() {
+		map.keySet();
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void putShouldThrowException() {
+		map.put(null, null);
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void putAllShouldThrowException() {
+		map.putAll(null);
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void removeShouldThrowException() {
+		map.remove(null);
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void sizeShouldThrowException() {
+		map.size();
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void valueShouldThrowException() {
+		map.values();
 	}
 
 }
