06ad115e85e2965e7aa46b374a7dc40304960c3f
Denilson Telaroli <ddts80@gmail.com>
2014-06-25 22:55:03 -0300

    Refactoring interceptor to ResourceBundle producer

--- src/test/java/br/com/caelum/vraptor/i18n/I18nLocalizationTest.java
+++ src/test/java/br/com/caelum/vraptor/i18n/I18nLocalizationTest.java
@@ -2,7 +2,6 @@ package br.com.caelum.vraptor.i18n;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -24,12 +23,14 @@ import br.com.caelum.vraptor.environment.Environment;
 import br.com.caelum.vraptor.http.MutableRequest;
 
 @SuppressWarnings("deprecation") 
-public class LocaleInterceptorTest {
+public class I18nLocalizationTest {
 
-	LocaleInterceptor interceptor;
 	@Mock MutableRequest request;
-	HttpSession session;
 	@Mock Environment env;
+	@Mock ServletContext context;
+	HttpSession session;
+	I18nLocalization i18n;
+	private Object defaultLocale;
 	
 	class MockHttpSession implements HttpSession {
 		private HashMap<String, Object> params = new HashMap<>();
@@ -65,117 +66,59 @@ public class LocaleInterceptorTest {
 		initMocks(this);
 
 		session = new MockHttpSession();
+		defaultLocale = Locale.getDefault();
 		when(request.getSession()).thenReturn(session);
-		when(request.getLocale()).thenReturn(new Locale("pt"));
+		when(request.getServletContext()).thenReturn(context);
 		when(env.get(anyString(), anyString())).thenReturn("true");
-		interceptor = new LocaleInterceptor(request, env);
+		i18n = new I18nLocalization(request, env);
 	}
 
-	@Test
-	public void shouldSetLocaleFromBrowserIfNull() {
-		assertNull(getLocaleFromSession());
-		
-		interceptor.setLocale();
-		Locale locale = getLocaleFromSession();
-		assertNotNull(locale);
-		assertEquals("pt", locale.getLanguage());
+	private void assertDefaultLocale() {
+		assertEquals(defaultLocale, i18n.getLocale());
 	}
 	
-	@Test
-	public void shouldNotSetLocaleFromBrowserIfNotNull() {
-		setLocaleEn();
-		
-		assertNotNull(getLocaleFromSession());
-		
-		interceptor.setLocale();
-		Locale locale = getLocaleFromSession();
-		assertNotNull(locale);
-		assertEquals("en", locale.getLanguage());
-	}
-
 	private void setLocaleEn() {
 		session.setAttribute(Config.FMT_LOCALE + ".session", new Locale("en"));
 	}
 	
 	@Test
 	public void shouldNotSetLocaleFromParameterIfNull() {
-		assertNull(getLocaleFromSession());
+		when(request.getParameter("_locale")).thenReturn(null);
+		assertDefaultLocale();
 		setLocaleEn();
 		
-		interceptor.setLocale();
-		Locale locale = getLocaleFromSession();
+		i18n.getBundle();
+		Locale locale = getLocale();
 		assertNotNull(locale);
 		assertEquals("en", locale.getLanguage());
 	}
 	
 	@Test
 	public void shouldSetLocaleFromParameterIfExists() {
-		when(request.getParameter("_locale")).thenReturn("pt");
-		assertNull(getLocaleFromSession());
-		setLocaleEn();
-		assertEquals(getLocaleFromSession().getLanguage(), "en");
-		
-		interceptor.setLocale();
-		Locale locale = getLocaleFromSession();
-		assertNotNull(locale);
-		assertEquals("pt", locale.getLanguage());
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void shouldNotSetLocaleIfInvalidParameter() {
-		when(request.getParameter("_locale")).thenReturn("12-12");
-		assertNull(getLocaleFromSession());
-		setLocaleEn();
-		assertEquals(getLocaleFromSession().getLanguage(), "en");
-		
-		interceptor.setLocale();
-	}
-	
-	@Test
-	public void shouldReplaceLocaleFromIsoToTag() {
 		when(request.getParameter("_locale")).thenReturn("pt_BR");
-		assertNull(getLocaleFromSession());
+		assertDefaultLocale();
 		setLocaleEn();
-		assertEquals(getLocaleFromSession().getLanguage(), "en");
+		assertEquals(getLocale().getLanguage(), "en");
 		
-		interceptor.setLocale();
-		Locale locale = getLocaleFromSession();
+		i18n.getBundle();
+		Locale locale = getLocale();
 		assertNotNull(locale);
 		assertEquals("pt", locale.getLanguage());
-		assertEquals("BR", locale.getCountry());
-		
-		when(request.getParameter("_locale")).thenReturn("pt-br");
-		interceptor.setLocale();
-		locale = getLocaleFromSession();
-		assertNotNull(locale);
-		assertEquals("pt", locale.getLanguage());
-		assertEquals("BR", locale.getCountry());
-	}
-
-	@Test
-	public void shouldDisableFeatureSetFromBrowserByEnv() {
-		when(env.get("locale.enableFromBrowser", "false")).thenReturn("false");
-		when(env.get("locale.enableFromParameter", "false")).thenReturn("false");
-		assertNull(getLocaleFromSession());
-		
-		interceptor.setLocale();
-		assertNull(getLocaleFromSession());
 	}
 	
 	@Test
 	public void shouldDisableFeatureSetFromParameterByEnv() {
-		when(env.get("locale.enableFromBrowser", "false")).thenReturn("false");
 		when(env.get("locale.enableFromParameter", "true")).thenReturn("false");
 		when(request.getParameter("_locale")).thenReturn("pt");
 		
-		assertNull(getLocaleFromSession());
+		assertDefaultLocale();
 		
-		interceptor.setLocale();
-		assertNull(getLocaleFromSession());
+		i18n.getBundle();
+		assertDefaultLocale();
 	}
 	
-	private Locale getLocaleFromSession() {
-		return (Locale) Config.get(session, Config.FMT_LOCALE);
+	private Locale getLocale() {
+		return i18n.getLocale();
 	}
 
 }
